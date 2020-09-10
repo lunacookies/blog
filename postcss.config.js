84fb367d4eca80e4d371f14b708aaab4382096f5
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-09-10 20:10:29 +1000

    Fix bug where generated compatibility CSS wasn’t minified

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   'plugins': [
-    require('cssnano')({
-      'preset': 'default',
-    }),
     require('postcss-preset-env'),
     require('postcss-sorting')({
       'properties-order': 'alphabetical',
+    }),
+    require('cssnano')({
+      'preset': 'default',
     }),
   ],
 };
